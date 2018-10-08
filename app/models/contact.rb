39e227c1f22214c70e14bf1886b6aaa90ae670ec
Asma Laaribi <laaribi.asm@gmail.com>
2018-10-08 00:11:15 -0400

    fixed the homepage

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,10 @@
 class Contact < MailForm::Base
-  attribute :name, :validate => true
+  attribute :name, :validate => true, length: { minimum: 2 }
   attribute :phone, :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
   attribute :nickname,  :captcha  => true
+
 
   def headers
     {
