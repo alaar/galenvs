0d6b9457e54dc352bfca9c1eb64bf6f471be325b
Asma Laaribi <laaribi.asm@gmail.com>
2018-10-08 10:52:55 -0400

    added map

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < MailForm::Base
   attribute :name, :validate => true, length: { minimum: 2 }
-  attribute :phone, :validate => true
+  attribute :phone, :validate => /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/,
+                               message: I18n.t('global.errors.phone_format')
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
   attribute :nickname,  :captcha  => true
